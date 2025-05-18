046b90876292ffd0b1193bc707eff66b1e881d43
ikopmniko <hannapitman786@gmail.com>
2025-05-18 20:44:18 +0700

    Update run3.py

--- run3.py
+++ run3.py
@@ -21,10 +21,10 @@ def read_csv_range(filename, start, end):
         rows = [row[0] for i, row in enumerate(csv.reader(f)) if start <= i < end]
     return rows
 
-start_row = 750
-end_row = 1000
+start_row = 1000
+end_row = 1250
 
-email = "xuheykrsul"
+email = "ElisAgniaWijaya"
 password = "@@Eskepal123"
 
 # Baca judul video sesuai rentang yang diinginkan
